475d7be0fded51cdc75bc7013c067ce51ff8f4a4
Michael Constantinou <michaelconstantinou330@gmail.com>
2018-06-09 20:22:19 +0100

    whitelisted name params and added greeting to homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def first_name
+    self.name.split.first
+  end
+
 end
