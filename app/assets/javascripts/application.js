1e1ce0d6b1048b87c003804a02de021b14c487f5
Michael Constantinou <michaelconstantinou330@gmail.com>
2018-06-09 20:47:47 +0100

    added gems and modified setup for bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,11 +5,14 @@
 // vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
+// compiled file. JavaScrß    ipt code in this file should be added after the last require_* statement.
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require bootstrap-sprocketsß
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
