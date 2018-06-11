6837065c9bcef070259b064b10c14f399e8335b3
Michael Constantinou <michaelconstantinou330@gmail.com>
2018-06-11 21:04:48 +0100

    tidied code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -71,10 +71,6 @@ class TasksController < ApplicationController
     end
   end
 
-  def toggle
-
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_task
--- app/controllers/toggle_controller.rb
+++
@@ -1,8 +0,0 @@
-class ToggleController < ApplicationController
-
-  def toggle_tasks
-    @tasks = Task.all.where(completed: true)
-    redirect_to tasks_path, notice: "Tasks were successfully toggled"
-  end
-
-end
