f193df34cef4eda8232fbd0c156614046bcf3170
Michael Constantinou <michaelconstantinou330@gmail.com>
2018-06-13 09:14:21 +0100

    name and description cannot be blank

--- db/migrate/20180610162843_create_tasks.rb
+++ db/migrate/20180610162843_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
       t.references :user, foreign_key: true
-      t.string :description
+      t.string :description, null: false
       t.boolean :completed, default: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20180610162843) do
 
   create_table "tasks", force: :cascade do |t|
     t.integer "user_id"
-    t.string "description"
+    t.string "description", null: false
     t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180610162843) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -32,7 +33,6 @@ ActiveRecord::Schema.define(version: 20180610162843) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
